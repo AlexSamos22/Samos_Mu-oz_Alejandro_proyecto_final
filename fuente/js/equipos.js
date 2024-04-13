cc1004cdd795c4bbfb10d1a48f0c9d9fc73b0a12
AlexSamos22 <asammun767@g.educaand.es>
2024-04-13 12:34:09 +0200

    cambios en equipos

--- fuente/js/equipos.js
+++ fuente/js/equipos.js
@@ -19,13 +19,13 @@ async function obtenerEquipos() {
         });
     });
 
-    // Crea la tabla de campeones
-    const tablaCampeones = crearTabla(campeones);
-    document.getElementById('equipos-finalistas').appendChild(tablaCampeones);
+    // Crea las tarjetas de campeones
+    const tarjetasCampeones = crearTarjeta(campeones);
+    document.getElementById('equipos-finalistas').innerHTML = tarjetasCampeones;
 
-    // Crea la tabla de no campeones
-    const tablaNoCampeones = crearTabla(noCampeones);
-    document.getElementById('resto-equipos').appendChild(tablaNoCampeones);
+    // Crea las tarjetas de no campeones
+    const tarjetasNoCampeones = crearTarjeta(noCampeones);
+    document.getElementById('resto-equipos').innerHTML = tarjetasNoCampeones;
 
     /*
     setTimeout(() => {
@@ -35,62 +35,41 @@ async function obtenerEquipos() {
     */
 }
 
-function crearTabla(datos) {
-    // Crea la tabla y el encabezado
-    const tabla = document.createElement('table');
-    tabla.style.width = '100%';
-    tabla.className = 'table-auto border-collapse border border-green-800 m-3'; // Clases de Tailwind para la tabla
-    const encabezado = document.createElement('thead');
-    const filaEncabezado = document.createElement('tr');
-    const titulos = ['Pais', 'Creado por', 'Resultado', 'Equipo', 'Exportar'];
+function crearTarjeta(datos) {
+    let tarjetasHtml = '';
 
-    titulos.forEach(titulo => {
-        const celdaEncabezado = document.createElement('th');
-        celdaEncabezado.textContent = titulo;
-        celdaEncabezado.className = 'p-2 bg-red-500 text-white';
-        filaEncabezado.appendChild(celdaEncabezado);
-    });
-
-    encabezado.appendChild(filaEncabezado);
-    tabla.appendChild(encabezado);
-
-    // Crea el cuerpo de la tabla
-    const cuerpo = document.createElement('tbody');
-
-    // Crea una cadena de HTML para las filas
-    let filasHtml = '';
-    datos.forEach((campo) => {
+    datos.forEach(campo => {
         // Crea la cadena de HTML para las imágenes de los Pokémon
-        // Crea la cadena de HTML para las imágenes de los Pokémon
-        let imagenesPokemonHtml = '<div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-3">';
+        let imagenesPokemonHtml = '<div class="grid grid-cols-3  gap-3 w-full justify-items-center items ">';
         for (let i = 1; i <= 6; i++) {
-            imagenesPokemonHtml += `<img src="${campo[`P${i}`]}" class="w-full h-auto">`;
+            imagenesPokemonHtml += `<img src="${campo[`P${i}`]}" class="w-2/5 h-auto">`;
         }
         imagenesPokemonHtml += '</div>';
 
-        // Añade la fila a la cadena de HTML
-        filasHtml += `
-            <tr class="bg-white even:bg-blue-100">
-                <td class="p-2 text-center align-middle w-modificado-5"><img src="${campo.Pais}" class="w-full h-auto"></td>
-                <td class="p-2 text-center align-middle"><p class="font-bold text-black">${campo.Autor}</p></td>
-                <td class="p-2 text-center align-middle">
-                    <div>
-                        <p style="color:${typeColors.rojo}">${campo.Torneo}</p>
-                        <p class="font-bold ">${campo.Posicion}</p>
-                        <p>(${campo.Fecha})</p>
-                    </div>
-                </td>
-                <td class="p-2 text-center align-middle w-2/5">${imagenesPokemonHtml}</td>
-                <td class="p-2 text-center align-middle"><i class="fas fa-file-export"></i></td>
-            </tr>
+        // Añade la tarjeta a la cadena de HTML
+        tarjetasHtml += `
+            <div class="p-4 rounded-lg shadow-md mb-4 flex flex-col justify-center items-center w-full bg-white border border-gray-200 space-y-2">
+                <div class="flex items-center justify-center  gap-3">
+                    <p class="font-bold text-black mr-2">${campo.Autor}</p>
+                    <img src="${campo.Pais}" class="w-modificado-5 h-auto">
+                </div>
+            
+                <div class=" m-4 flex flex-col justify-center items-center w-full">
+                    <p style="color:${typeColors.rojo}">${campo.Torneo}</p>
+                    <p>(${campo.Fecha})</p>
+                    <p class="font-bold mb">${campo.Posicion}</p>
+                </div>
+            
+                ${imagenesPokemonHtml}
+                <div class="flex justify-between items-center w-full mt-4">
+                    <button class="bg-blue-500 text-white p-2 rounded"><i class="fas fa-file-export"></i> Export</button>
+                    <button class="bg-red-500 text-white p-2 rounded"><img src="../img/icons8-corazón-pokemon-50.png" class="w-6 h-6"></button>
+                </div>
+            </div>
         `;
     });
 
-    // Añade todas las filas al cuerpo de la tabla de una sola vez
-    cuerpo.innerHTML = filasHtml;
-    tabla.appendChild(cuerpo);
-
-    return tabla;
+    return tarjetasHtml;
 }
 
 obtenerEquipos();
